e0d1b8db6128517006e8e660eae7d0c8824e08b3
John Rodney <johnrodney@Johns-iMac-2.local>
2015-04-06 16:18:23 -0500

    final build

--- src/main.js
+++ src/main.js
@@ -13,9 +13,12 @@ import matrices from './matrices/matrices';
 // the scripts to load jpm_packages/traceour(or6to5)-runtime.js
 // and build.js
 
-matrices.testLoader(null, function(){
-  React.render(<ToggleCalendar />, document.getElementById('toggle-target'));
-  React.render(<CalendarComponent name="jquery-calendar-left" />, document.getElementById('calendar-left'));
-  React.render(<CalendarComponent name="jquery-calendar-right" />, document.getElementById('calendar-right'));
-  $('.throbber').hide();
-});
+window.initiateCalendar = function(nid){
+  matrices.testLoader(null, function(){
+    React.render(<ToggleCalendar />, document.getElementById('toggle-target'));
+    React.render(<CalendarComponent name="jquery-calendar-left" />, document.getElementById('calendar-left'));
+    React.render(<CalendarComponent name="jquery-calendar-right" />, document.getElementById('calendar-right'));
+    $('.throbber').hide();
+  }, nid);
+};
+
--- src/matrices/matrices.js
+++ src/matrices/matrices.js
@@ -168,11 +168,11 @@ class matriceManager{
     }
   }
   // loads the matrices from the test server
-  testLoader(self, callback){
+  testLoader(self, callback, nid){
     var that = self || this;
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'http://5b45b4e6.ngrok.com/v1/20898/rent-matrix', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', 'http://5b45b4e6.ngrok.com/v1/'+nid+'/rent-matrix', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
